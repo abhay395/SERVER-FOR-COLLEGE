5d0202c3258cd732142212be8635541da247619f
Abhay prajapati <abhayparja90@gmail.com>
2024-10-04 16:42:11 +0530

    changes in cloude

--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,19 +1,24 @@
-const cloudinary= require('../config/cloudinaryConfig')
-const fs = require('fs')
+const cloudinary = require("../config/cloudinaryConfig");
+const fs = require("fs");
 
-const uploadOncloudinary=async(localFilePath,fileType)=>{
-    if(!localFilePath){
-        return null;
-    }
-  try {
-      const result = await cloudinary.uploader.upload(localFilePath, {
-          resource_type: fileType,
-        });
-        fs.unlinkSync(localFilePath)
-        return result;
-  } catch (error) {
-    fs.unlinkSync(localFilePath);
+const uploadOncloudinary = async (localFilePath, fileType) => {
+  if (!localFilePath) {
     return null;
   }
-}
-exports.uploadOncloudinary=uploadOncloudinary;
+  try {
+    const stream = cloudinary.uploader.upload_stream((error, result) => {
+      if (error) {
+        return res.status(500).send("Upload failed.");
+      }
+      res.status(200).send(result);
+    });
+
+    streamifier.createReadStream(req.file.buffer).pipe(stream);
+    // fs.unlinkSync(localFilePath);
+    return result;
+  } catch (error) {
+    // fs.unlinkSync(localFilePath);
+    return null;
+  }
+};
+exports.uploadOncloudinary = uploadOncloudinary;
